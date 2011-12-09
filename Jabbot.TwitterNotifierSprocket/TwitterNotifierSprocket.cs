504766eec2ab6e6b7f5982b6666da851dc573411
sethwebster <sethwebster@gmail.com>
2011-12-09 16:13:22 -0700

    It's aliiiive\!

--- Jabbot.TwitterNotifierSprocket/TwitterNotifierSprocket.cs
+++ Jabbot.TwitterNotifierSprocket/TwitterNotifierSprocket.cs
@@ -6,24 +6,29 @@ using Jabbot.Sprockets;
 using System.Text.RegularExpressions;
 using TweetSharp;
 using System.Configuration;
+using System.Collections.ObjectModel;
 
 namespace Jabbot.TwitterNotifierSprocket
 {
     public class TwitterNotifierSprocket : ISprocket
     {
         private static readonly Regex _usernameMatchRegex = new Regex(@"(@\w+)");
-       
         private static readonly string _tweetFormat = "{0}, you were just mentioned by @{1} here http://jabbr.net/#/rooms/{2}.";
+        private static ICollection<UserNotificationModel> _userNotifications = new Collection<UserNotificationModel>();
 
         public bool Handle(Models.ChatMessage message, Bot bot)
         {
             var twitterUsers = GetUserNamesFromMessage(message.Content);
             foreach (var u in twitterUsers)
             {
-                TweetSharp.TwitterService svc = new TwitterService(GetClientInfo());
-                svc.AuthenticateWith(ConfigurationManager.AppSettings["User.Token"],
-                    ConfigurationManager.AppSettings["User.TokenSecret"]);
-                svc.SendTweet(String.Format(_tweetFormat, u.ScreenName, message.FromUser, message.Room));
+                if (ShouldNotifyUser(u.ScreenName))
+                {
+                    TweetSharp.TwitterService svc = new TwitterService(GetClientInfo());
+                    svc.AuthenticateWith(ConfigurationManager.AppSettings["User.Token"],
+                        ConfigurationManager.AppSettings["User.TokenSecret"]);
+                    svc.SendTweet(String.Format(_tweetFormat, u.ScreenName, message.FromUser, message.Room));
+                    MarkUserNotified(u.ScreenName);
+                }
             }
             return true;
         }
@@ -42,7 +47,7 @@ namespace Jabbot.TwitterNotifierSprocket
                                 .Where(v => !String.IsNullOrEmpty(v.ScreenName));
         }
 
-        TwitterClientInfo GetClientInfo()
+        private TwitterClientInfo GetClientInfo()
         {
             return new TwitterClientInfo()
             {
@@ -50,5 +55,26 @@ namespace Jabbot.TwitterNotifierSprocket
                 ConsumerSecret = ConfigurationManager.AppSettings["Application.ConsumerSecret"]
             };
         }
+
+        private void MarkUserNotified(string UserName)
+        {
+            var userRec = _userNotifications.FirstOrDefault(u => u.UserName == UserName);
+            if (userRec == null)
+            {
+                userRec = new UserNotificationModel()
+                {
+                    UserName = UserName
+                };
+                _userNotifications.Add(userRec);
+            }
+            userRec.LastNotification = DateTime.Now;
+        }
+
+        private bool ShouldNotifyUser(string UserName)
+        {
+            return _userNotifications.FirstOrDefault(u => u.UserName == UserName) == null
+                || _userNotifications.FirstOrDefault(u => u.UserName == UserName && (DateTime.Now - u.LastNotification).TotalMinutes >= 5) != null;
+        }
+
     }
 }
---
+++ Jabbot.TwitterNotifierSprocket/UserNotificationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jabbot.TwitterNotifierSprocket
+{
+    public class UserNotificationModel
+    {
+        public string UserName { get; set; }
+        public DateTime LastNotification { get; set; }
+            
+    }
+}
