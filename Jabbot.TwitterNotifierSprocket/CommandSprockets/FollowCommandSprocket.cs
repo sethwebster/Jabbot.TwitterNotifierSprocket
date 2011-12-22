5f023a3c991983498ed3a86a8b8273717832aa3f
sethwebster <sethwebster@gmail.com>
2011-12-22 14:52:09 -0700

    Added Follow Command

---
+++ Jabbot.TwitterNotifierSprocket/CommandSprockets/FollowCommandSprocket.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using TweetSharp;
+using System.Configuration;
+using System;
+
+namespace Jabbot.TwitterNotifierSprocket.CommandSprockets
+{
+    public class FollowCommandSprocket : TwitterNotifierCommandSprocketBase
+    {
+        public override IEnumerable<string> SupportedCommands
+        {
+            get { yield return "follow"; }
+        }
+
+        public override bool ExecuteCommand()
+        {
+            if (Arguments.Length == 0)
+            {
+                throw new InvalidOperationException("Follow who?");
+            }
+            TweetSharp.TwitterService svc = new TwitterService(GetClientInfo());
+            svc.AuthenticateWith(ConfigurationManager.AppSettings["User.Token"],
+                ConfigurationManager.AppSettings["User.TokenSecret"]);
+            svc.FollowUser(Arguments[0]);
+            Bot.PrivateReply(Message.FromUser, "Ok -- I'm following " + Arguments[0]);
+            return true;
+        }
+
+        private TwitterClientInfo GetClientInfo()
+        {
+            return new TwitterClientInfo()
+            {
+                ConsumerKey = ConfigurationManager.AppSettings["Application.ConsumerKey"],
+                ConsumerSecret = ConfigurationManager.AppSettings["Application.ConsumerSecret"]
+            };
+        }
+    }
+}
