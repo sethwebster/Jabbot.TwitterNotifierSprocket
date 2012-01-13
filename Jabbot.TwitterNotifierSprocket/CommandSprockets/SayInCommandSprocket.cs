f109a2d4009d65cb9e39761e55ea33f872964520
sethwebster <sethwebster@gmail.com>
2012-01-12 21:50:47 -0700

    Added SayIn message and updated help text

---
+++ Jabbot.TwitterNotifierSprocket/CommandSprockets/SayInCommandSprocket.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace Jabbot.TwitterNotifierSprocket.CommandSprockets
+{
+    public class SayInCommandSprocket : TwitterNotifierCommandSprocketBase
+    {
+        public override IEnumerable<string> SupportedCommands
+        {
+            get { yield return "sayin"; }
+        }
+
+        public override bool ExecuteCommand()
+        {
+            if (Arguments.Length == 0)
+                throw new InvalidOperationException("You must provide a room name");
+            string room = Arguments[0];
+            Bot.Say(string.Join(" ", Arguments.Skip(1).ToArray()), room);
+            return true;
+        }
+    }
+}
