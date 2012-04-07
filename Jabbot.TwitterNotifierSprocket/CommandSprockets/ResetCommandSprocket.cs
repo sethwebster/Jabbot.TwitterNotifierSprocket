58c1e2c2ed9885cb89b12dd92a650a85675ab04f
Seth Webster <sethwebster@gmail.com>
2012-04-07 14:15:38 -0700

    updated for latest newtonsoFT json

---
+++ Jabbot.TwitterNotifierSprocket/CommandSprockets/ResetCommandSprocket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Jabbot.TwitterNotifierSprocket.CommandSprockets
+{
+    public class ResetCommandSprocket : TwitterNotifierAdminCommandSprocketBase
+    {
+        public override IEnumerable<string> SupportedCommands
+        {
+            get { yield return "reset"; }
+        }
+
+        public override bool ExecuteCommand()
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://twitterbot.apphb.com/bot/start");
+            req.GetResponse();
+            return true;
+        }
+    }
+}
